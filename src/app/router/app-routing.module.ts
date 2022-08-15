9ed9fc967feddee7062486606cf40d02addc3e86
benjaminbascary <benjaminbascary@gmail.com>
2022-08-15 11:20:47 -0300

    Moves routing-module into its own router folder
    Not much to say

--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ByCapitalComponent } from "./countries/pages/by-capital/by-capital.component";
+import { ByCapitalComponent } from "../countries/pages/by-capital/by-capital.component";
 
 // Components
-import { ByCountryComponent } from "./countries/pages/by-country/by-country.component";
-import { ByRegionComponent } from "./countries/pages/by-region/by-region.component";
-import { SeeCountryComponent } from "./countries/pages/see-country/see-country.component";
+import { ByCountryComponent } from "../countries/pages/by-country/by-country.component";
+import { ByRegionComponent } from "../countries/pages/by-region/by-region.component";
+import { SeeCountryComponent } from "../countries/pages/see-country/see-country.component";
 
 const routes: Routes = [
   {
