ecb616c986097cf43851dec620a6f76f3f840337
benjaminbascary <benjaminbascary@gmail.com>
2022-08-15 11:24:18 -0300

    Imports the AppRoutingModule into shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // Routing
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/app-routing.module';
 // Developer's
 import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
