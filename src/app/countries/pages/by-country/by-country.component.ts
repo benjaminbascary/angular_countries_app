d9922009ce43ee8dd9079d934cba361a9a1f96dd
benjaminbascary <benjaminbascary@gmail.com>
2022-08-15 12:15:16 -0300

    Build html and form logic to search a specific contry

--- src/app/countries/pages/by-country/by-country.component.ts
+++ src/app/countries/pages/by-country/by-country.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './by-country.component.html',
   styleUrls: ['./by-country.component.css']
 })
-export class ByCountryComponent implements OnInit {
+export class ByCountryComponent {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  searchTerm: string = '';
+  
+  handleSubmit() {
+    console.log(this.searchTerm)
   }
 
 }
