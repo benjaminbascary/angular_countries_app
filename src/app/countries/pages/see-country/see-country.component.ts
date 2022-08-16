92f8516087a1de03104ced835778bb034ce8997e
benjaminbascary <benjaminbascary@gmail.com>
2022-08-16 14:49:04 -0300

    Builds logic to get a single country with alpha code

--- src/app/countries/pages/see-country/see-country.component.ts
+++ src/app/countries/pages/see-country/see-country.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Country } from '../../interfaces/search-country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-see-country',
-  templateUrl: './see-country.component.html',
-  styleUrls: ['./see-country.component.css']
+  templateUrl: './see-country.component.html'
 })
 export class SeeCountryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private SearchCountryService: CountryService 
+  ) { }
+
+  country: Country[] = [];
+  errorMessage: string = '';
+  error: Boolean = false;
 
   ngOnInit(): void {
+    this.ActivatedRoute.params
+      .subscribe(({id}) => {
+        this.error = false; 
+        this.SearchCountryService.searchCountryByCode(id)
+          .subscribe(response => {
+            console.log(response)
+            this.country = response;
+          }, (error) => {
+            this.error = true;
+            this.errorMessage = `Can't find any country matching ${id}`;
+            this.country = [];
+          })
+      })
   }
 
 }
