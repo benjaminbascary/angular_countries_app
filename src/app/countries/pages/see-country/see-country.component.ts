e23656a1dfd56baaadf0f0683b4f044051dd05d2
benjaminbascary <benjaminbascary@gmail.com>
2022-08-16 16:02:52 -0300

    Finishes the see-country component

--- src/app/countries/pages/see-country/see-country.component.ts
+++ src/app/countries/pages/see-country/see-country.component.ts
@@ -5,12 +5,25 @@ import { ActivatedRoute } from '@angular/router';
 import { Country } from '../../interfaces/search-country.interface';
 import { CountryService } from '../../services/country.service';
 // rxjs
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 
 @Component({
   selector: 'app-see-country',
-  templateUrl: './see-country.component.html'
+  templateUrl: './see-country.component.html',
+  styles: [
+    `
+      .info-container {
+        margin: 10px;
+        padding: 20px;
+        border-radius: 5px;
+        border: 1px solid gray;
+      }
+      img {
+        width: 10rem;
+      }
+    `
+  ]
 })
 export class SeeCountryComponent implements OnInit {
 
@@ -19,17 +32,20 @@ export class SeeCountryComponent implements OnInit {
     private SearchCountryService: CountryService 
   ) { }
 
-  country: Country[] = [];
+  country!: Country;
   errorMessage: string = '';
   error: Boolean = false;
+  translations: any = [];
 
   ngOnInit(): void {
     this.ActivatedRoute.params
       .pipe(
-        switchMap(({id}) => this.SearchCountryService.searchCountryByCode(id))
+        switchMap(({id}) => this.SearchCountryService.searchCountryByCode(id)),
+        tap(console.log)
       )
       .subscribe(response => {
-        this.country = response;
+        this.country = response[0];
+        this.translations = response[0].translations
       })
   }
 
