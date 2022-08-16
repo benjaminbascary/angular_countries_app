7f242fe03a5b62b241795d32510030307ed21073
benjaminbascary <benjaminbascary@gmail.com>
2022-08-16 13:46:06 -0300

    Finishes with by region search feature

--- src/app/countries/pages/by-region/by-region.component.ts
+++ src/app/countries/pages/by-region/by-region.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/search-country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styleUrls: ['./by-region.component.css']
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
 
-  constructor() { }
+  constructor(private SearchCountryService: CountryService) {}
 
-  ngOnInit(): void {
+  error: boolean = false;
+  errorMessage: string = ''
+  searchTerm: string = '';
+  countries: Country[] = [];
+ 
+  // This term comes from /
+  handleSubmit(term: string) {
+    this.error = false; 
+    this.SearchCountryService.searchCountryByRegion(term)
+      .subscribe(response => {
+        this.countries = response;
+      }, (error) => {
+        this.error = true;
+        this.errorMessage = `Can't find any country matching ${term}`;
+        this.countries = [];
+      })
+  }
+
+  suggestions(searchTerm?: string) {
+    this.error = false
+    /** 
+      @TODO crear suggestions!
+    */
   }
 
 }
--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -13,6 +13,7 @@ export class CountryService {
   private BASE_URL: string = "https://restcountries.com/v3.1";
   private BY_NAME: string = "name"; 
   private BY_CAPITAL: string = "capital";
+  private BY_REGION: string = "region";
 
   constructor(private http: HttpClient) { }
  
@@ -23,6 +24,11 @@ export class CountryService {
 
   searchCountryByCapital(searchTerm: string): Observable<Country[]> {
     const URL = `${this.BASE_URL}/${this.BY_CAPITAL}/${searchTerm}`
+    return this.http.get<Country[]>(URL);
+  }
+
+  searchCountryByRegion(searchTerm: string) {
+    const URL = `${this.BASE_URL}/${this.BY_REGION}/${searchTerm}`
     return this.http.get<Country[]>(URL);
   }
 }
