77ec981a86fe5773456c29149e3be937786d3deb
benjaminbascary <benjaminbascary@gmail.com>
2022-08-16 10:48:40 -0300

    Finishes implementation to search by capital

--- src/app/countries/components/search-input/search-input.component.ts
+++ src/app/countries/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { debounceTime, Subject } from 'rxjs';
 
 @Component({
@@ -20,7 +20,7 @@ export class SearchInputComponent implements OnInit {
   }
 
   
-  
+  @Input() placeHolder: string = '';
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();
